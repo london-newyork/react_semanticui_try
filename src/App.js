5fdb2b66e181e81e66dbc76557b6e06bc1b3e6f6
y inoue <admin@wainoMacBook-Pro.local>
2021-09-12 23:28:41 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import Counter from './components/Counter';
+
+import Button from './Button';
+
+const App = () => {
+
+    const [resources, setResources] = useState([]);
+
+    return (
+    <div className='ui container' style={{ marginTop: '30px' }}>
+        <div>App</div>
+        <Button title='post'/>
+        <Button title='edit'/>
+        <Button title='delete'/>
+        <Button title='submit'/>
+        <Counter />
     </div>
-  );
-}
+    );
+};
 
 export default App;
